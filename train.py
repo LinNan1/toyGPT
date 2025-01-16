efd7423bf0e0bae65d887f48002fb71822b2bbc8
LinNan <linnan1@foxmail.com>
2025-01-16 12:32:29 +0800

    init

---
+++ train.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+import torch
+import time
+
+dataset = 'tinyshakespeare'
+data_dir = os.path.join('data', dataset)
+batch_size = 12
+block_size = 1024
+
+def get_batch(split):
+    if split == 'train':
+        data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
+    else:
+        data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
+    y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
+    return x, y
+
+X, Y = get_batch('train')
+t0 = time.time()
